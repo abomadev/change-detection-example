85a24673a7c1ae21faa6685bc7fdfa01903a2efb
abomadev <aboma.kelly@gmail.com>
2022-09-08 14:31:41 +0930

    comment out

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -7,11 +7,11 @@ import { Item } from '../parent/child-two/child-two.component';
 })
 export class DataService {
   items: Item[] = [
-    {id: 1, title: "Item 1"},
-    {id: 2, title: "Item 2"},
-    {id: 3, title: "Item 3"},
-    {id: 4, title: "Item 4"},
-    {id: 5, title: "Item 5"}
+    // {id: 1, title: "Item 1"},
+    // {id: 2, title: "Item 2"},
+    // {id: 3, title: "Item 3"},
+    // {id: 4, title: "Item 4"},
+    // {id: 5, title: "Item 5"}
   ]
 
   items$: BehaviorSubject<Item[]> = new BehaviorSubject<Item[]>(this.items)
